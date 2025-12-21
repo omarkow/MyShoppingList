bc5195162b637d71707775a35434a3efe56bd7b7
Olivier Markowitch <olivier.markowitch@icloud.com>
2025-12-21 18:06:18 +0100

    Initial Commit

---
+++ MyShoppingListTests/MyShoppingListTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyShoppingListTests.swift
+//  MyShoppingListTests
+//
+//  Created by Olivier Markowitch on 21/12/2025.
+//
+
+import Testing
+
+struct MyShoppingListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
